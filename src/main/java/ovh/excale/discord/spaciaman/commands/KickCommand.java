906983fe35da4eb6f20ecc2c7032141e054c71ac
excale <escaccia@live.it>
2021-02-04 12:54:17 +0100

    Hibernate configuration

--- src/main/java/ovh/excale/discord/spaciaman/commands/KickCommand.java
+++ src/main/java/ovh/excale/discord/spaciaman/commands/KickCommand.java
@@ -5,7 +5,9 @@ import com.jagrosh.jdautilities.command.CommandEvent;
 import net.dv8tion.jda.api.entities.Invite;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
+import ovh.excale.discord.spaciaman.SpaciaMan;
 
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 public class KickCommand extends Command {
@@ -17,6 +19,7 @@ public class KickCommand extends Command {
 		this.help = "Kick someone for fun!";
 		this.arguments = "<@user>";
 		// Set command cooldown to 30m
+		this.cooldown = SpaciaMan.debug() ? 0 : 1800;
 		this.guildOnly = true;
 	}
 
@@ -38,7 +41,21 @@ public class KickCommand extends Command {
 				.retrieveInvites()
 				.queue(invites -> {
 
-//					invites.stream().filter(Invite::isExpanded).filter(invite -> invite);
+					Invite invite = null;
+					for(Invite inv : invites)
+						if(inv.isExpanded() && !inv.isTemporary() && inv.getMaxUses() == 0 && inv.getMaxAge() == 0) {
+							invite = inv;
+							break;
+						}
+
+					if(invite == null) {
+						event.reply(
+								"You have to create an invite first with no max uses, no expire date and not temporary.");
+						return;
+					}
+
+
+
 
 				});
 
---
+++ src/main/java/ovh/excale/discord/spaciaman/models/KickRepository.java
@@ -0,0 +1,21 @@
+package ovh.excale.discord.spaciaman.models;
+
+import net.dv8tion.jda.api.entities.User;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class KickRepository {
+
+
+
+	public @Nullable UserModel getUser(@NotNull User user) {
+		return getUser(user.getIdLong());
+	}
+
+	public @Nullable UserModel getUser(long snowflake) {
+
+		return null;
+
+	}
+
+}
