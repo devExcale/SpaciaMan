f44a6f656d9f1ce09432c6875f16a46c4ed8b7bd
excale <escaccia@live.it>
2021-02-05 14:35:05 +0100

    Added JUnit to POM

---
+++ src/test/java/ovh/excale/test/UsersTest.java
@@ -0,0 +1,13 @@
+package ovh.excale.test;
+
+import org.junit.jupiter.api.Test;
+import ovh.excale.discord.spaciaman.models.UserModel;
+
+public class UsersTest {
+
+	@Test
+	void insert_user_test() {
+
+	}
+
+}
