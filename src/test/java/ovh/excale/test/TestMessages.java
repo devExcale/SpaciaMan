18f065b0ac3f26bf753e7a7db381c0d2ba785165
excale <dev_excale@hotmail.com>
2021-02-07 02:12:36 +0100

    Doesn't work

---
+++ src/test/java/ovh/excale/test/TestMessages.java
@@ -0,0 +1,38 @@
+package ovh.excale.test;
+
+import org.junit.jupiter.api.*;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import ovh.excale.discord.spaciaman.UserRepository;
+import ovh.excale.discord.spaciaman.models.UserModel;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class TestMessages {
+
+	private static final Logger logger = LoggerFactory.getLogger(TestMessages.class);
+
+	@Test
+	void try_insert() {
+
+		UserModel user = new UserModel().setSnowflake(123L)
+				.setNickname("testuser");
+
+		UserRepository repo = new UserRepository();
+		repo.saveUser(user);
+
+		Optional<UserModel> optional = repo.getUser(123L);
+
+		assertTrue(optional.isPresent());
+		assertEquals("testuser",
+				optional.get()
+						.getNickname());
+
+	}
+
+}
